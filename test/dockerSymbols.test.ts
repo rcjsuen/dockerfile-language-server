66b5469f5cceac762b6cde25c78ac341392549ad
Remy Suen <remy.suen@gmail.com>
2017-06-26 06:20:06 +0900

    WIP #3 Add tests for document symbols
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

---
+++ test/dockerSymbols.test.ts
@@ -0,0 +1,77 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Remy Suen. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import * as child_process from "child_process";
+import * as assert from "assert";
+
+import {
+	TextEdit, TextDocument, Position, Range, SymbolKind,
+	Command, ExecuteCommandParams,
+	Diagnostic, DiagnosticSeverity
+} from 'vscode-languageserver';
+import { ValidationCode } from '../src/dockerValidator';
+import { DockerSymbols } from '../src/dockerSymbols';
+
+let uri = "uri://host/Dockerfile.sample";
+let symbolsProvider = new DockerSymbols();
+
+function createDocument(content: string): any {
+	return TextDocument.create("uri://host/Dockerfile.sample", "dockerfile", 1, content);
+}
+
+function createRange(startLine, startCharacter, endLine, endCharacter): Range {
+	return Range.create(Position.create(startLine, startCharacter), Position.create(endLine, endCharacter));
+}
+
+describe("Dockerfile document symbols", function () {
+	describe("whitespace", function() {
+		it("empty file", function () {
+			let document = createDocument("");
+			let symbols = symbolsProvider.parseSymbolInformation(document, uri);
+			assert.equal(symbols.length, 0);
+		});
+	});
+
+	describe("directives", function() {
+		it("escape directive", function () {
+			let document = createDocument("#escape=`");
+			let symbols = symbolsProvider.parseSymbolInformation(document, uri);
+			assert.equal(symbols.length, 1);
+			assert.equal(symbols[0].containerName, undefined);
+			assert.equal(symbols[0].name, "escape");
+			assert.equal(symbols[0].kind, SymbolKind.Property);
+			assert.equal(symbols[0].location.uri, uri);
+			assert.equal(symbols[0].location.range.start.line, 0);
+			assert.equal(symbols[0].location.range.start.character, 1);
+			assert.equal(symbols[0].location.range.end.line, 0);
+			assert.equal(symbols[0].location.range.end.character, 6);
+		});
+
+		it("space", function () {
+			let document = createDocument("# escape=`");
+			let symbols = symbolsProvider.parseSymbolInformation(document, uri);
+			assert.equal(symbols.length, 1);
+			assert.equal(symbols[0].containerName, undefined);
+			assert.equal(symbols[0].name, "escape");
+			assert.equal(symbols[0].kind, SymbolKind.Property);
+			assert.equal(symbols[0].location.uri, uri);
+			assert.equal(symbols[0].location.range.start.line, 0);
+			assert.equal(symbols[0].location.range.start.character, 2);
+			assert.equal(symbols[0].location.range.end.line, 0);
+			assert.equal(symbols[0].location.range.end.character, 7);
+
+			document = createDocument("#\tescape=`");
+			symbols = symbolsProvider.parseSymbolInformation(document, uri);
+			assert.equal(symbols.length, 1);
+			assert.equal(symbols[0].containerName, undefined);
+			assert.equal(symbols[0].name, "escape");
+			assert.equal(symbols[0].kind, SymbolKind.Property);
+			assert.equal(symbols[0].location.uri, uri);
+			assert.equal(symbols[0].location.range.start.line, 0);
+			assert.equal(symbols[0].location.range.start.character, 2);
+			assert.equal(symbols[0].location.range.end.line, 0);
+			assert.equal(symbols[0].location.range.end.character, 7);
+		});
+	});
+});
