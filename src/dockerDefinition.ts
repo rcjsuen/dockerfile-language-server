b56265c5d866c1b0bd9c54b7d2e79c52afbe0665
Remy Suen <remy.suen@gmail.com>
2017-07-09 08:04:12 +0900

    Reorder the code so FROMs are iterated over only once
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- src/dockerDefinition.ts
+++ src/dockerDefinition.ts
@@ -12,13 +12,6 @@ export class DockerDefinition {
 	public computeDefinition(document: TextDocument, position: Position): Location | null {
 		let parser = new DockerfileParser();
 		let dockerfile = parser.parse(document);
-		for (let instruction of dockerfile.getFROMs()) {
-			let range = instruction.getBuildStageRange();
-			if (range && range.start.line === position.line && range.start.character <= position.character && position.character <= range.end.character) {
-				return Location.create(document.uri, range);
-			}
-		}
-
 		let source = undefined;
 		for (let instruction of dockerfile.getCOPYs()) {
 			let range = instruction.getFromValueRange();
@@ -28,12 +21,11 @@ export class DockerDefinition {
 			}
 		}
 
-		if (source) {
-			for (let instruction of dockerfile.getFROMs()) {
-				if (instruction.getBuildStage() === source) {
-					let range = instruction.getBuildStageRange();
-					return Location.create(document.uri, range);
-				}
+		for (let instruction of dockerfile.getFROMs()) {
+			let range = instruction.getBuildStageRange();
+			if (range &&
+					((range.start.line === position.line && range.start.character <= position.character && position.character <= range.end.character) || (instruction.getBuildStage() === source))) {
+				return Location.create(document.uri, range);
 			}
 		}
 
