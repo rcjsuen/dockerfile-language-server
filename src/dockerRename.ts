ba697ad3e9c7cb1d12814c5c37dadea445288d78
Remy Suen <remy.suen@gmail.com>
2017-07-11 21:46:04 +0900

    WIP #51 Remove unnecessary code
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- src/dockerRename.ts
+++ src/dockerRename.ts
@@ -13,22 +13,8 @@ export class DockerRename {
 		let edits = [];
 		let highlighter = new DockerHighlight();
 		let highlightRanges = highlighter.computeHighlightRanges(document, position);
-		if (highlightRanges.length !== 0) {
-			let content = document.getText();
-			for (let highlightRange of highlightRanges) {
-				let start = document.offsetAt(highlightRange.range.start);
-				let end = document.offsetAt(highlightRange.range.end);
-				let target = content.substring(start, end);
-				if (target.indexOf("${") === 0) {
-					let range = Range.create(document.positionAt(start + 2), document.positionAt(end - 1));
-					edits.push(TextEdit.replace(range, newName));
-				} else if (target.indexOf('$') === 0) {
-					let range = Range.create(document.positionAt(start + 1), highlightRange.range.end);
-					edits.push(TextEdit.replace(range, newName));
-				} else {
-					edits.push(TextEdit.replace(highlightRange.range, newName));
-				}
-			}
+		for (let highlightRange of highlightRanges) {
+			edits.push(TextEdit.replace(highlightRange.range, newName));
 		}
 		return edits;
 	}
