95fc6bf1d3e0713db8cde9ea82028e87a4cc73a2
Remy Suen <remy.suen@gmail.com>
2017-07-16 11:25:22 +0900

    Add a missing license header
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- src/dockerValidatorSettings.ts
+++ src/dockerValidatorSettings.ts
@@ -1,3 +1,7 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Remy Suen. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 import { ValidationSeverity } from './dockerValidator';
 
 export interface ValidatorSettings {
